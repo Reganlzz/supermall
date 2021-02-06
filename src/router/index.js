7666165e4f57e7e1a45a548094bbc2e75d6159f1
regan <15311092959@163.com>
2021-02-06 16:40:33 +0800

    新内容

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+const Home = () => import('../views/home/Home')
+const Category = () => import('../views/category/Category')
+const ShopCart = () => import('../views/shopcart/ShopCart')
+const Profile = () => import('../views/profile/Profile')
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: '',
+    redirect: '/home',
+  },
+  {
+    path: '/home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/cart',
+    component: ShopCart
+  },
+  {
+    path: '/profile',
+    component: Profile
   }
 ]
 
